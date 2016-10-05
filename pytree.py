da648ef5d02e56d3acc6a1a71425a2811657fc4f
Clara Shim <seohyuns@andrew.cmu.edu>
2016-10-04 23:45:31 -0400

    really no whitespace?i couldn't leave old code for reference? booo

--- pytree.py
+++ pytree.py
@@ -3,29 +3,11 @@ import os
 import re
 import sys
 
-# rootdir = os.getcwd()
-
-
-# for root, dirs, files in os.walk("."):
-#     path = root.split('/')
-#     files = [f for f in files if not f[0] == '.']
-#     dirs[:] = [d for d in dirs if not d[0] == '.']
-#     for dir in dirs[:-1]:
-#         print( (len(path) - 1) * '   ', '├──',os.path.basename(root))
-#     for dir in dirs[-1:]:      
-#         print( (len(path) - 1) * '   ', '└──',os.path.basename(root))
-#     for file in files[:-1]:  
-#         print( len(path) * '   ', '├──',file)  
-#     for file in files[-1:]:      
-#         print( len(path) * '   ', '└──',file)  
-
 numDir = 0
 numFile = 0
 
-
 def sort(file):
     return re.sub('[^A-Za-z0-9]+', '', file).lower()
-
 
 def sortedfiles_without_hidden(root):
     dir = []
@@ -33,7 +15,6 @@ def sortedfiles_without_hidden(root):
         if not i.startswith("."):
             dir.append(i)
     return sorted(dir, key=sort)
-
 
 def tree(root, prefix=""):
     files = sortedfiles_without_hidden(root)
@@ -54,8 +35,6 @@ def tree(root, prefix=""):
         else:
             numFile+= 1
 
-
-
 if len(sys.argv) == 1:
     cwd = os.getcwd()
     print(".")
@@ -64,17 +43,3 @@ else:
     print(cwd)
 count = tree(cwd)
 print(str(numDir) + " directories, " + str(numDir) + " files")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
